b0b8ce56ddb02020ec1a77455fbd5ed5940c58ea
joaoguilherme09 <joaoguipmendes>
2026-05-28 18:48:22 -0300

    render port

--- run.py
+++ run.py
@@ -7,4 +7,9 @@ print(os.getcwd())
 print(app.template_folder)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
