694eb7fea361f6963ffa7c02e18b635c77b49f57
joaoguilherme09 <joaoguipmendes>
2026-06-03 17:36:55 -0300

    atualização mobile 3.0

--- app/static/js/menu.js
+++ app/static/js/menu.js
@@ -4,14 +4,43 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const mobileButton = document.querySelector(".mobile-menu-button");
-    const mobileMenu = document.querySelector(".menu");
+    const mobileButton =
+        document.querySelector(".mobile-menu-button");
+
+    const mobileMenu =
+        document.querySelector(".menu");
 
     if (mobileButton) {
 
         mobileButton.addEventListener("click", () => {
 
             mobileMenu.classList.toggle("active");
+
+        });
+
+    }
+
+    // =========================
+    // DROPDOWN MOBILE
+    // =========================
+
+    const dropdownLink =
+        document.querySelector(".dropdown > a");
+
+    const dropdown =
+        document.querySelector(".dropdown");
+
+    if (dropdownLink && dropdown) {
+
+        dropdownLink.addEventListener("click", (e) => {
+
+            if (window.innerWidth <= 768) {
+
+                e.preventDefault();
+
+                dropdown.classList.toggle("active");
+
+            }
 
         });
 
