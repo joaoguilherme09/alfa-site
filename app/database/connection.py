47bfa4f65b04bb1bf9c99a55a920e4f27582b600
joaoguilherme09 <joaoguipmendes@gmail.com>
2026-06-25 18:44:07 -0300

    add outro banco

--- app/database/connection.py
+++ app/database/connection.py
@@ -6,16 +6,11 @@ def create_connection():
     try:
 
         connection = mysql.connector.connect(
-
-            host="zephyr.proxy.rlwy.net",
-
-            user="root",
-
-            password="mesNlDsQeXxAYRfWdTOJOMvBvHRWixsE",
-
-            database="railway",
-
-            port=19496
+            host="auth-db1576.hstgr.io",
+            user="u726483483_alfa_user",
+            password="Alfa25062026profissionalizantes",
+            database="u726483483_alfa_portal",
+            port=3306
 
         )
 
